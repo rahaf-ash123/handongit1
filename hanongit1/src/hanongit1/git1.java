da02d5de9789d289081eec961241bd38bd9ec9bb
922641 <922641@LTSA000384.cts.com>
2021-08-02 16:28:41 +0300

    check out ok

--- hanongit1/src/hanongit1/git1.java
+++ hanongit1/src/hanongit1/git1.java
@@ -1,7 +1,12 @@
 package hanongit1;
 
 public class git1 {
-int []a; int []b;
-
+int []a; 
+public void nn2(){
+	System.out.println("nn2");
+}
+public void nn3(){
+	System.out.println("nn3");
+}
 
 }
